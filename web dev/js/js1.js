86cbf974d561274baab4b456ab754f69d6a2957b
atharva29 <atharvaajgaonkar29@gmail.com>
2018-01-01 09:47:30 +0530

    Add files via upload

---
+++ web dev/js/js1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+  class number extends React.Component{
+render()  {
+   return (this.props.val + 1 );
+  }
+}
+ReactDOM.render(
+  <number val=
+);
